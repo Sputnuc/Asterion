62e6006234fab11895567c9d3fa5cfbb1e430c33
Sputnuc <asdasdasdboom123@gmail.com>
2024-10-22 22:49:23 +0500

    Add files via upload

---
+++ scripts/distribution.js
@@ -0,0 +1,16 @@
+const whitoriteDuct = extend(Duct, "whitorite-duct", {});
+
+const whitoriteDuctBridge = extend(DuctBridge, "whitorite-duct-bridge", {});
+
+const whitoriteJunction = extend(Junction, "whitorite-junction", {});
+
+const whitoriteRouter = extend(Router, "whitorite-router", {});
+
+const whitoriteSorter = extend(Sorter, "whitorite-sorter", {});
+
+const whitoriteInvertedSorter = extend(Sorter, "whitorite-inverted-sorter", {});
+
+const whitoriteInvertedGateway = extend(OverflowGate, "whitorite-inverted-gateway", {});
+
+
+
