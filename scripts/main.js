8b85518362dc1a009cbd06dc6ea7e7d53f0fcf0f
Sputnuc <asdasdasdboom123@gmail.com>
2024-10-22 23:07:54 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,15 @@ require("blocks/small-craters");
 require("attributes");
 require("units");
 require("distribution");
+require("crafting");
+require("production");
+require("turret");
+require("wall");
+require("power");
+require("storage");
+require("liquids");
+require("payload");
+require("storage");
+require("effect");
+require("other");
+
