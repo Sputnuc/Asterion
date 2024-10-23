3df50d979593d28d953eb02ea627e2907067f9db
Sputnuc <asdasdasdboom123@gmail.com>
2024-10-23 11:31:31 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,3 @@ require("blocks/small-craters");
 require("attributes");
 require("units");
 require("distribution");
-require("block/crafting");
-require("blocks/production");
-require("blocks/turret");
-require("blocks/wall");
-require("blocks/power");
-require("blocks/storage");
-require("blocks/liquids");
-require("blocks/payload");
-require("blocks/storage");
-require("blocks/effect");
-require("blocks/other");
-
