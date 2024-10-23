41a229389b4ae409695ae52f2f07a5a7bdf1df25
Sputnuc <asdasdasdboom123@gmail.com>
2024-10-23 18:36:04 +0500

    Update distribution.js

--- scripts/distribution.js
+++ scripts/distribution.js
@@ -1,17 +1,10 @@
 extend(Duct, "whitorite-duct", {});
-
 extend(DuctBridge, "whitorite-duct-bridge", {});
-
 extend(Junction, "whitorite-junction", {});
-
 extend(Router, "whitorite-router", {});
-
 extend(Sorter, "whitorite-sorter", {});
-
 extend(Sorter, "whitorite-inverted-sorter", {});
-
 extend(OverflowGate, "whitorite-gateway", {});
-
 extend(OverflowGate, "whitorite-inverted-gateway", {});
 
 
