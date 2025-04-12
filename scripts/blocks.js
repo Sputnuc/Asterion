862761743bdcb5fc9fcf1cf9ba6eb2ddac4bbe6e
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-04-13 00:18:42 +0300

    Update by Sputnuc
    All work that was made by Sputnuc

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,3 +1,7 @@
+// Логистика. Logistic
+// Взят пример из sapphirum
+//пример: const "кодовое название предмета" = extend("Класс/type", "в кавычках название блока json", {});
+
 const whitoriteDuct = extend(Duct, "whitorite-duct", {});
 
 const whitoriteDuctBridge = extend(DuctBridge, "whitorite-duct-bridge", {});
@@ -14,4 +18,35 @@ const whitoriteGateway = extend(OverflowGate, "whitorite-gateway", {});
 
 const whitoriteInvertedGateway = extend(OverflowGate, "whitorite-inverted-gateway", {});
 
+const magnetiteConveyor = extend(StackConveyor,"magnetite-conveyor", {});
 
+
+// Турели. Turrets
+
+const splinter = extend(ItemTurret, "splinter", {});
+
+const trasher = extend(ItemTurret, "trasher", {});
+
+const breakthrough = extend(ItemTurret, "breakthrough", {});
+
+const glare = extend(ContinuousLiquidTurret, "glare", {});
+
+const spark = extend(PowerTurret, "spark", {});
+
+const lrml = extend(ItemTurret, "LRML", {});
+
+const centuary = extend(ItemTurret, "centuary",{});
+
+const infection = extend(TractorBeamTurret, "infection", {});
+
+const transition = extend(ItemTurret, "transition", {});
+
+const intermediary = extend(ItemTurret, "intermediary", {});
+
+const supernova = extend(PowerTurret, "supernova", {});
+
+const typhoon = extend(ItemTurret, "typhoon", {});
+
+const volcano = extend(ItemTurret, "volcano", {});
+
+const terror = extend(ItemTurret, "terror", {});
