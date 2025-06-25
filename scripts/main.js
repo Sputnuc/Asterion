f390923bf42cbfd1fd689e858ac3a737184b2efc
Sputnuc <asdasdasdboom123@gmail.com>
2025-06-25 22:29:53 +0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,16 @@ require("asAttributes");
 require("asUnits");
 require("asBlocks");
 require("asTurrets");
+
+let yourlasttick = Vars.tree.loadMusic("yourlasttick");
+let ascendancy = Vars.tree.loadMusic("ascendancy");
+let fearintheair = Vars.tree.loadMusic("fearintheair");
+let importantTerritory = Vars.tree.loadMusic("importantTerritory");
+
+
+Events.on(ClientLoadEvent, e => {
+	Vars.control.sound.darkMusic.add(yourlasttick);
+	Vars.control.sound.ambientMusic.add(fearintheair);
+	Vars.control.sound.ambientMusic.add(importantTerritory);
+	Vars.control.sound.bossMusic.add(ascendancy);
+});
